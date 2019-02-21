3b56fe2a4adb3e794e70f3241ce3f71a14ddafe2
Paul Bennings <paul.bennings@codeimmersives.com>
2019-02-21 14:57:31 -0500

    added changes

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 window.onload = init;
-
 function init() {
     document.querySelector('#input')
-        .addEventListener('keydown', handleTyping);
+        .addEventListener('keyup', handleTyping);
 }
 
 function handleTyping(event) {
-    let userInput = event.target.value;
-    let answer = userInput;
-    
-    document.querySelector('#changeme').innerText = answer;
+    let userInput = event.target.value.toString();
+    // let answer = userInput;
+    // myString will make sure we start every word starts of lowercase
+    let myString = userInput.toLowerCase().split(' ');
+
+    // we're using a loop to find the first letter in the word
+    // i =0 means were we start
+    // i < myString.length means where we end
+    // i = i + 1 means how were incrementing 
+    for(let i = 0; i < myString.length; i = i+1){
+        myString[i] = myString[i].charAt(0).toUpperCase() + myString[i].slice(1);
+    }
+
+    document.querySelector('#output').innerText = myString.join(' ')
 }
