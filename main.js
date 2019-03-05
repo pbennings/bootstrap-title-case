674f7fbc89a005555ab7372df1f6a0e15bd92c38
Paul Bennings <paul.bennings@codeimmersives.com>
2019-03-05 09:55:45 -0500

    stuff

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-window.onload = init;
+ window.onload = init;
 function init() {
     document.querySelector('#input')
         .addEventListener('keyup', handleTyping);
@@ -6,17 +6,33 @@ function init() {
 
 function handleTyping(event) {
     let userInput = event.target.value.toString();
-    // let answer = userInput;
-    // myString will make sure we start every word starts of lowercase
+    let answer = userInput;
+            // myString will make sure we start every word starts of lowercase
     let myString = userInput.toLowerCase().split(' ');
 
-    // we're using a loop to find the first letter in the word
-    // i =0 means were we start
-    // i < myString.length means where we end
-    // i = i + 1 means how were incrementing 
+            // we're using a loop to find the first letter in the word
+            // i =0 means were we start
+            // i < myString.length means where we end
+            // i = i + 1 means how were incrementing 
     for(let i = 0; i < myString.length; i = i+1){
         myString[i] = myString[i].charAt(0).toUpperCase() + myString[i].slice(1);
     }
 
-    document.querySelector('#output').innerText = myString.join(' ')
+    document.querySelector('#changeme').innerText = myString.join(' ');
+
+
+// if statement
+// let i = 0;
+   
+//     if(userInput === ' ' ){
+//         userInput[i].toUpperCase();
+
+//     }
+//     else {
+        
+//     }
+
+//     document.querySelector('#changeme').innerText = answer;
+// 
 }
+
